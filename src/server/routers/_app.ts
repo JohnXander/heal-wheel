6be2417b2da0a3013207a9e62adf705fce8802b3
John Xander <johnblox5@gmail.com>
2022-12-11 13:25:53 +0100

    test test endpoint 2

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -6,9 +6,11 @@ export const appRouter = router({
   createUser: procedure
     .input(z.object({ name: z.string() }))
     .mutation(async ({ input }) => {
-      const createdUser = await prisma.user.create({ data: { ...input } })
+      const createdUser = await prisma.user.create({
+        data: { ...input }
+      });
       return { createdUser }
-    })
+    }),
 });
 
 export type AppRouter = typeof appRouter;

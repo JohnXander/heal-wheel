595480589133ae88392c22e49e6deeff9e46b38b
John Xander <johnblox5@gmail.com>
2022-12-14 18:50:22 +0100

    change saved message style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import { trpc } from '../utils/trpc';
 import { useState } from 'react';
 import Head from 'next/head';
 import { Wheel } from '../components/Wheel';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
 
 export default function IndexPage() {
   const userQuery = trpc.getUser.useQuery("clbmnqiej00007kjcvnrladne");
@@ -28,16 +30,19 @@ export default function IndexPage() {
       <main className='bg-gray-800 w-screen h-screen flex flex-col justify-center items-center'>
 
         <Wheel user={user} setUser={setUser} setSavedStats={setSavedStats} />
-        <div className='m-4'></div>
         {!savedStats && <button
-          className='border-2 border-green-400 text-green-400 rounded p-2 hover:text-white hover:border-green-500 hover:bg-green-500'
+          className='border-2 border-green-400 text-green-400 rounded p-2 hover:text-white hover:border-green-500 hover:bg-green-500 my-6'
           onClick={handleSave}>
           SAVE STATS
         </button>}
         {savedStats && <h1
-          className='border-2 border-gray-800 text-green-500 p-2 pointer-events-none'
+          className='border-2 border-gray-800 text-green-500 pointer-events-none mt-4'
           onClick={handleSave}>
           SAVED
+          <FontAwesomeIcon
+            className='cursor-pointer text-green-400 text-xs'
+            icon={faCheck}
+          />
         </h1>}
       </main>
 

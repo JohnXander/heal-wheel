52d15ed828f3dbfe4ab43217048f7a8ca67ee8c1
John Xander <johnblox5@gmail.com>
2022-12-15 17:07:47 +0100

    restyle advice page

--- src/components/Advice.tsx
+++ src/components/Advice.tsx
@@ -14,32 +14,40 @@ interface AdviceProps {
 export const Advice: FC<AdviceProps> = ({ user }) => {
     return (
         <div>
-            <div className='w-80 h-80 lg:w-96 lg:h-96 flex justify-center items-center relative'>
-                <div className='bg-blue-400 w-40 h-40 lg:w-48 lg:h-48 absolute left-0 top-0 flex flex-col items-center'>
-                    <h1>Mind</h1>
-                    <p>You rated {user?.mind} out of 5</p>
-                    <button className="border-2 p-2">See advice</button>
+            <div className='w-80 h-80 lg:w-96 lg:h-96 flex justify-center items-center relative rounded border-2'>
+                <div className='border-r-2 border-b-2 w-40 h-40 lg:w-48 lg:h-48 absolute left-0 top-0 flex flex-col items-center justify-center'>
+                    <div className="bg-blue-200 flex flex-col items-center justify-center py-2 px-4 rounded">
+                        <h1 className="text-2xl">Mind</h1>
+                        <p>Rating: {user?.mind} out of 5</p>
+                        <button className="py-2 px-4 bg-blue-300 rounded my-2">See advice</button>
+                    </div>
                 </div>
-                <div className='bg-green-400 w-40 h-40 lg:w-48 lg:h-48 absolute right-0 top-0 flex flex-col items-center'>
-                    <h1>Body</h1>
-                    <p>You rated {user?.body} out of 5</p>
-                    <button className="border-2 p-2">See advice</button>
+                <div className='border-b-2 w-40 h-40 lg:w-48 lg:h-48 absolute right-0 top-0 flex flex-col items-center justify-center'>
+                    <div className="bg-green-200 flex flex-col items-center justify-center py-2 px-4 rounded">
+                        <h1 className="text-2xl">Body</h1>
+                        <p>Rating: {user?.body} out of 5</p>
+                        <button className="py-2 px-4 bg-green-300 rounded my-2">See advice</button>
+                    </div>
                 </div>
-                <div className='bg-yellow-400 w-40 h-40 lg:w-48 lg:h-48 absolute bottom-0 left-0 flex flex-col items-center'>
-                    <h1>Social</h1>
-                    <p>You rated {user?.social} out of 5</p>
-                    <button className="border-2 p-2">See advice</button>
+                <div className='border-r-2 w-40 h-40 lg:w-48 lg:h-48 absolute bottom-0 left-0 flex flex-col items-center justify-center'>
+                    <div className="bg-yellow-200 flex flex-col items-center justify-center py-2 px-4 rounded">
+                        <h1 className="text-2xl">Social</h1>
+                        <p>Rating: {user?.social} out of 5</p>
+                        <button className="py-2 px-4 bg-yellow-300 rounded my-2">See advice</button>
+                    </div>
                 </div>
-                <div className='bg-red-400 w-40 h-40 lg:w-48 lg:h-48 absolute bottom-0 right-0 flex flex-col items-center'>
-                    <h1>Work</h1>
-                    <p>You rated {user?.work} out of 5</p>
-                    <button className="border-2 p-2">See advice</button>
+                <div className='w-40 h-40 lg:w-48 lg:h-48 absolute bottom-0 right-0 flex flex-col items-center justify-center'>
+                    <div className="bg-red-200 flex flex-col items-center justify-center py-2 px-4 rounded">
+                        <h1 className="text-2xl">Work</h1>
+                        <p>Rating: {user?.work} out of 5</p>
+                        <button className="py-2 px-4 bg-red-300 rounded my-2">See advice</button>
+                    </div>
                 </div>
             </div>
             <div className='flex justify-center'>
                 <button
                     className='border-2 border-green-400 text-green-400 rounded p-2 hover:text-white hover:border-green-500 hover:bg-green-500 my-8'>
-                    SAVE STATS
+                    HELLO
                 </button>
             </div>
         </div>

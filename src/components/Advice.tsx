30a4521c25e8116ddbf3e7c40a333ea1ed244ac2
John Xander <johnblox5@gmail.com>
2022-12-15 22:00:17 +0100

    add stars to ratings

--- src/components/Advice.tsx
+++ src/components/Advice.tsx
@@ -1,4 +1,6 @@
-import { Dispatch, FC, SetStateAction } from "react";
+import { FC } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 interface AdviceProps { 
     user: {
@@ -18,29 +20,69 @@ export const Advice: FC<AdviceProps> = ({ user }) => {
                 <div className='border-r-2 border-b-2 w-40 h-40 lg:w-48 lg:h-48 absolute left-0 top-0 flex flex-col items-center justify-center'>
                     <div className="bg-blue-200 flex flex-col items-center justify-center py-2 px-4 rounded">
                         <h1 className="text-2xl">Mind</h1>
-                        <p>Rating: {user?.mind} out of 5</p>
-                        <button className="py-2 px-4 bg-blue-300 rounded my-2">See advice</button>
+                        <div className="flex gap-x-0.5 pt-1 pb-3">
+                            {Array(user?.mind).fill("_").map((_, idx) => {
+                                return (
+                                    <FontAwesomeIcon
+                                        className="w-4 text-blue-500"
+                                        key={idx}
+                                        icon={faStar}
+                                    />
+                                )
+                            })}
+                        </div>
+                        <button className="py-2 px-4 bg-blue-300 rounded mb-2">See advice</button>
                     </div>
                 </div>
                 <div className='border-b-2 w-40 h-40 lg:w-48 lg:h-48 absolute right-0 top-0 flex flex-col items-center justify-center'>
                     <div className="bg-green-200 flex flex-col items-center justify-center py-2 px-4 rounded">
                         <h1 className="text-2xl">Body</h1>
-                        <p>Rating: {user?.body} out of 5</p>
-                        <button className="py-2 px-4 bg-green-300 rounded my-2">See advice</button>
+                        <div className="flex gap-x-0.5 pt-1 pb-3">
+                            {Array(user?.body).fill("_").map((_, idx) => {
+                                return (
+                                    <FontAwesomeIcon
+                                        className="w-4 text-green-500"
+                                        key={idx}
+                                        icon={faStar}
+                                    />
+                                )
+                            })}
+                        </div>
+                        <button className="py-2 px-4 bg-green-300 rounded mb-2">See advice</button>
                     </div>
                 </div>
                 <div className='border-r-2 w-40 h-40 lg:w-48 lg:h-48 absolute bottom-0 left-0 flex flex-col items-center justify-center'>
                     <div className="bg-yellow-200 flex flex-col items-center justify-center py-2 px-4 rounded">
                         <h1 className="text-2xl">Social</h1>
-                        <p>Rating: {user?.social} out of 5</p>
-                        <button className="py-2 px-4 bg-yellow-300 rounded my-2">See advice</button>
+                        <div className="flex gap-x-0.5 pt-1 pb-3">
+                            {Array(user?.social).fill("_").map((_, idx) => {
+                                return (
+                                    <FontAwesomeIcon
+                                        className="w-4 text-yellow-500"
+                                        key={idx}
+                                        icon={faStar}
+                                    />
+                                )
+                            })}
+                        </div>
+                        <button className="py-2 px-4 bg-yellow-300 rounded mb-2">See advice</button>
                     </div>
                 </div>
                 <div className='w-40 h-40 lg:w-48 lg:h-48 absolute bottom-0 right-0 flex flex-col items-center justify-center'>
                     <div className="bg-red-200 flex flex-col items-center justify-center py-2 px-4 rounded">
                         <h1 className="text-2xl">Work</h1>
-                        <p>Rating: {user?.work} out of 5</p>
-                        <button className="py-2 px-4 bg-red-300 rounded my-2">See advice</button>
+                        <div className="flex gap-x-0.5 pt-1 pb-3">
+                            {Array(user?.work).fill("_").map((_, idx) => {
+                                return (
+                                    <FontAwesomeIcon
+                                        className="w-4 text-red-500"
+                                        key={idx}
+                                        icon={faStar}
+                                    />
+                                )
+                            })}
+                        </div>
+                        <button className="py-2 px-4 bg-red-300 rounded mb-2">See advice</button>
                     </div>
                 </div>
             </div>

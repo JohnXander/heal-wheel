382a8452a8937465d36272d393d965f2e44eda08
John Xander <johnblox5@gmail.com>
2022-12-20 17:51:03 +0100

    add initial style to register component

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,5 +1,21 @@
 export const Register = ({ }) => {
     return (
-        <div>Register</div>
+        <form className="bg-blue-200 w-80 rounded flex flex-col justify-content items-center gap-4 p-4">
+            <h1 className="text-4xl">Sign Up</h1>
+            <input
+                className="p-3 w-5/6 rounded text-2xl outline-none"
+                placeholder="username"
+                type="text"
+            />
+            <input
+                className="p-3 w-5/6 rounded text-2xl outline-none"
+                placeholder="password"
+                type="password"
+            />
+            <input
+                className="bg-blue-400 py-3 px-6 rounded text-2xl"
+                type="submit"
+            />
+        </form>
     )
 }

a8236296c25ac68ac63eb00e937ccd79435c754d
John Xander <johnblox5@gmail.com>
2022-12-20 18:01:37 +0100

    add poppins

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,19 @@
 import '../styles/globals.css'
 import type { AppType } from 'next/app';
 import { trpc } from '../utils/trpc';
+import { Poppins } from '@next/font/google'
+
+const poppins = Poppins({
+  weight: '400',
+  subsets: ['latin'],
+})
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <main className={poppins.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 };
 
 export default trpc.withTRPC(MyApp);
